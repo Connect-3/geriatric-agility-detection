189fa7793ce0a0941673453c108bf2fe05dfe5e0
ratherSimple1 <rathersimple1@gmail.com>
2020-11-19 13:47:25 +0530

    Add box for detection

--- test.py
+++ test.py
@@ -3,7 +3,20 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-r = g = b = 0
+# r = g = b = 0
+
+# List = [['Geeks', 'For'] , ['Geeks']]
+
+n = 15
+
+store_colors = [[[0]*n]*n]*3
+
+box_range = 20
+
+start_x = 0
+end_x = 0
+start_y = 0
+end_y = 0
 
 while True:
 
@@ -13,9 +26,19 @@ while True:
 
     height, width, channels = frame.shape
 
-    b = frame[width//2, height//2, 0]
-    g = frame[width//2, height//2, 1]
-    r = frame[width//2, height//2, 2]
+    
+
+    store_colors = frame[start_x:(end_x+1), start_y:(end_y+1), 0:3]
+
+
+    # b = frame[width//2, height//2, 0]
+    # g = frame[width//2, height//2, 1]
+    # r = frame[width//2, height//2, 2]
+
+    start_x = width//2 - box_range
+    end_x = width//2 + box_range
+    start_y = height//2 - box_range
+    end_y = height//2 + box_range
 
     # print(r, g, b)
 
@@ -23,22 +46,39 @@ while True:
     if k == 27:
         break
 
-print(r, g, b)
+# print(r, g, b)
 
+# print(store_colors)
+
+print("Background registered")
+
+while True:
+    _, frame = cap.read()
+
+    cv2.imshow("panel", frame)
+    k = cv2.waitKey(30)
+    if k == 27:
+        break
+
+print("Timer started")
 
 cnt = 0
 
-range = 100
+check_range = 10
 
-def check(check_r, check_g, check_b):
-    if(check_r >= r-range and check_r <= r+range and check_g >= g-range and check_g <= g+range and check_b >= b-range and check_b <= b+range ) :
+def check(x, y):
+    if(x >= y-check_range and x <= y+check_range ) :
         return True
     else:
         return False
 
 last = False
 
+frames_skipped = 0
+
 while True:
+
+    
 
     _, frame = cap.read()
 
@@ -46,24 +86,41 @@ while True:
 
     height, width, channels = frame.shape
 
-    temp_b = frame[width//2, height//2, 0]
-    temp_g = frame[width//2, height//2, 1]
-    temp_r = frame[width//2, height//2, 2]
+    # temp_b = frame[width//2, height//2, 0]
+    # temp_g = frame[width//2, height//2, 1]
+    # temp_r = frame[width//2, height//2, 2]
+
+    if frames_skipped < 5:
+        frames_skipped += 1
+        continue
 
     # print(r, g, b)
-    current = check(temp_r, temp_g, temp_b)
-    
+
+    temp_store_colors = frame[start_x:(end_x+1), start_y:(end_y+1), 0:3]
+
+    current = True
+
+    for i in range(0, 11):
+        for j in range(0, 11):
+            for k in range(0, 3):
+                current = current and check(temp_store_colors[i][j][k], store_colors[i][j][k])
+
+    # current = check(temp_b, temp_g, temp_r)
+    print("Checking")
     if(current != last):
         cnt += 1
+        print("Count increased")
 
     last = current
+
+    frames_skipped = 0
 
     k = cv2.waitKey(30)
     if k == 27:
         break
 
 
-print("The answer is", cnt)
+print("The answer is", cnt//2)
 
-print(cnt // 2)
+print("Count is", cnt)
 
