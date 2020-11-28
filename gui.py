937f575b9a8c4b45ac5a73f5c335fbd439567259
enti-re <nikhilchandna2000@gmail.com>
2020-11-28 12:38:35 +0530

    connecting test.py to gui.py

--- gui.py
+++ gui.py
@@ -5,6 +5,36 @@ import sys
 from PyQt5.QtWidgets import * 
 from PyQt5.QtGui import QImage, QIcon, QPixmap, QPalette, QBrush, QColor, QFontDatabase, QFont
 from PyQt5.QtCore import *
+from PyQt5 import QtCore
+from test import *
+# import cv2
+# import numpy as np
+
+# cap = cv2.VideoCapture(0)
+
+#Play variables
+# box_range = 30
+# check_range = 40
+
+
+# n = box_range*2+10
+# store_colors = [[[]*n]*n]*3
+
+# _, frame = cap.read()
+
+# height, width, channels = frame.shape
+
+# #Center
+# start_x = width//2 - box_range
+# end_x = width//2 + box_range
+# start_y = height//2 - box_range
+# end_y = height//2 + box_range
+
+# #Moving box up
+# start_y -= 200
+# end_y -= 200
+
+# cnt = 0
 
 
 class MainWindow(QWidget):
@@ -13,22 +43,17 @@ class MainWindow(QWidget):
         super(MainWindow, self).__init__()
 
         self.layout = QVBoxLayout()
-        # self.label = QLabel("Geriatric Agility Detection")
-        # self.label.setAlignment(QtCore.Qt.AlignCenter)
-        # self.label.move(200,500)
         label = QLabel("Geriatric Agility Detection", self)
         label.setStyleSheet("color: #CFD8DC")
-        # label.setChecked(label.editor.fontUnderline())
         label.move(105,105)
+
         label.setFont(QFont('Fredoka One', 29))
-        # label.setStyleSheet(“background-color: blue;”)
         self.setStyleSheet("""
         background-color: #5D6D7E;
         
         """)
         label.adjustSize()
 
-        # self.layout.addWidget(self.label)
         self.setWindowTitle("My Own Title")
         self.setLayout(self.layout)
 
@@ -39,35 +64,20 @@ class MainWindow(QWidget):
 
     def Button1(self): 
 
-        # creating a push button 
-        button = QPushButton("Start", self) 
-        # button.setFont(QFont('Comic Sans MS', 20,)
-        button.setFixedSize(100,35)
+        button = QPushButton("Start", self)
         button.move(290,185)
-        button.setFont(QFont("Fredoka One",13.5))
-        # button.setStyleSheet("color: ")
+        # button.setFont(QFont("Fredoka One",13.5))
         button.setStyleSheet("""
         background-color: #80CBC4;
         color: #FDFEFE;
         border-radius: 5px;
+        left: 100px;
         """)
-        # button.setStyleSheet("border-radius: 1")
-        # button.setStyleSheet("border-color: blue")
-        # button.setStyleSheet("border-color: #373a40")
-        #  button.setStyleSheet("border-radius : 50;  
-                            #   border : 2px solid black") 
+        button.clicked.connect(register_Background)
 
-
-        
-        # setting geometry of button 
-        # button.setGeometry(200, 150, 100, 30) 
-
-        # adding action to a button 
-        button.clicked.connect(self.clickme) 
 
     def Button2(self): 
 
-        # creating a push button 
         button = QPushButton("Guide", self) 
         button.setFont(QFont("Fredoka One",13.5))
         button.setFixedSize(100,35)
@@ -79,17 +89,12 @@ class MainWindow(QWidget):
         """)
 
         
-        # setting geometry of button 
-        # button.setGeometry(200, 150, 100, 30) 
 
-        # adding action to a button 
         button.clicked.connect(self.clickme)
 
     def Button3(self): 
 
-        # creating a push button 
         button = QPushButton("Exit", self) 
-        # button.setFixedSize(100,0)
         button.setFont(QFont("Fredoka One",13.5))
         button.setFixedSize(100,35)
         button.move(290,295)
@@ -100,20 +105,23 @@ class MainWindow(QWidget):
         """)
 
         
-        # setting geometry of button 
-        # button.setGeometry(200, 150, 100, 30) 
 
-        # adding action to a button 
         button.clicked.connect(self.clickme) 
 
     def clickme(self): 
         # printing pressed 
         print("pressed")
 
+    def registerBackground(self): 
+        # printing pressed 
+        print("pressed")
+
 
 if __name__ == "__main__":
+    # cap = cv2.VideoCapture(0)
     app = QApplication(sys.argv)
     mw = MainWindow()
     mw.resize(700,450)
+    mw.setFixedSize(700, 450)
     mw.show()
     sys.exit(app.exec_())
--- test.py
+++ test.py
@@ -1,113 +1,13 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
-
-# r = g = b = 0
-
-# List = [['Geeks', 'For'] , ['Geeks']]
-
-
-
-
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) #captureDevice = camera
 box_range = 30
-
 n = box_range*2+10
-
-store_colors = [[[]*n]*n]*3
-
-start_x = 0
-end_x = 0
-start_y = 0
-end_y = 0
-
-_, frame = cap.read()
-
-height, width, channels = frame.shape
-
-
-start_x = width//2 - box_range
-end_x = width//2 + box_range
-start_y = height//2 - box_range
-# start_y = height//2 - box_range//2
-
-
-end_y = height//2 + box_range
-
-start_y -= 200
-end_y -= 200
-
-# start_y -= 
-
-
-# cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (255,0,0), 2)
-
-
-
-
-
-while True:
-
-    _, frame = cap.read()
-
-    # height, width, channels = frame.shape
-
-    cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (255, 0, 0), 2)
-    # cv2.rectangle(frame, (start_x-50, start_y-50), (end_x+50, end_y+50), (255,255,0), 2)
-    
-
-
-    cv2.imshow("panel", frame)
-
-    
-
-    store_colors = frame[ start_y:(end_y+1) , start_x:(end_x+1), 0:3]
-
-
-    # b = frame[width//2, height//2, 0]
-    # g = frame[width//2, height//2, 1]
-    # r = frame[width//2, height//2, 2]
-
-    
-
-
-
-    # print(r, g, b)
-
-    k = cv2.waitKey(30)
-    if k == 27:
-        break
-
-# print(r, g, b)
-
-# print(store_colors.shape)
-# print(store_colors)
-
-# print(frame.shape)
-
-print("Background registered")
-
-while True:
-    _, frame = cap.read()
-
-
-    cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (255,0,0), 2)
-    # cv2.rectangle(frame, (start_x-50, start_y-50), (end_x+50, end_y+50), (255,255,0), 2)
-
-
-    cv2.imshow("panel", frame)
-    cv2.imshow("color taken", store_colors)
-    k = cv2.waitKey(30)
-    if k == 27:
-        break
-
-
-
-print("Timer started")
-
-cnt = 0
-
 check_range = 40
+# last = True
+# cnt = 0
 
 def check(x, y):
     if(x >= y-check_range and x <= y+check_range ) :
@@ -115,76 +15,81 @@ def check(x, y):
     else:
         return False
 
-last = True
+store_colors = [[[]*n]*n]*3
 
-frames_skipped = 0
+_, frame = cap.read()
+# cap.release()
+height, width, channels = frame.shape
 
-while True:
+start_x = width//2 - box_range
+end_x = width//2 + box_range
+start_y = height//2 - box_range
+end_y = height//2 + box_range
+start_y -= 200
+end_y -= 200
+
+
+
+# print(last)
+
+
+def register_Background():
+    
+    
+    while True:
+        _, frame = cap.read()
+        cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (255, 0, 0), 2)
+        cv2.imshow("panel", frame)
+        store_colors = frame[ start_y:(end_y+1) , start_x:(end_x+1), 0:3]
+        k = cv2.waitKey(30)
+        if k == 27:
+            print("Background registered")
+            cv2.destroyAllWindows()
+            break
+
+
+    while True:
+        _, frame = cap.read()
+        cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (255,0,0), 2)
+        cv2.imshow("panel", frame)
+        cv2.imshow("color taken", store_colors)
+        k = cv2.waitKey(30)
+        if k == 27:
+            break
+    print("Timer started")
+    last = True
+    cnt=0
+    while True:
+        _, frame = cap.read()
+        cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (255,0,0), 2)
+        cv2.imshow("panel", frame)
+        height, width, channels = frame.shape
+        cv2.rectangle(frame, (start_x, start_y), (end_x+1, end_y+1), (255,0,0), 2)
+        temp_store_colors = frame[ start_y:(end_y+1) , start_x:(end_x+1), 0:3]
+        current = True
+        for i in range(0, box_range*2):
+            for j in range(0, box_range*2):
+                for k in range(0, 3):
+                    current = current and check(temp_store_colors[i][j][k], store_colors[i][j][k])
+
+        cv2.imshow("Checking", temp_store_colors)
+        print("Checking")
+        if(current != last):
+            cnt += 1
+            print(frame[width//2, height//2])
+            print("Count increased")
+        last = current
+        k = cv2.waitKey(30)
+        if k == 27:
+            break
+
+    print("The answer is", cnt//2)
+    print("Count is", cnt)
+    cv2.destroyAllWindows()
 
     
 
-    _, frame = cap.read()
-
-    cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (255,0,0), 2)
-    # cv2.rectangle(frame, (start_x-50, start_y-50), (end_x+50, end_y+50), (255,255,0), 2)
 
 
 
-    # cv2.circle(frame, (width//2, height//2), 2, (0, 0, 255), 2)
-
-    
-    cv2.imshow("panel", frame)
-
-    height, width, channels = frame.shape
-
-    # temp_b = frame[width//2, height//2, 0]
-    # temp_g = frame[width//2, height//2, 1]
-    # temp_r = frame[width//2, height//2, 2]
-
-    cv2.rectangle(frame, (start_x, start_y), (end_x+1, end_y+1), (255,0,0), 2)
-
-    # if frames_skipped < 1:
-    #     frames_skipped += 1
-    #     continue
-
-    # print(r, g, b)
-
-    temp_store_colors = frame[ start_y:(end_y+1) , start_x:(end_x+1), 0:3]
-
-    # print(temp_store_colors.shape)
-
-    current = True
-    for i in range(0, box_range*2):
-        for j in range(0, box_range*2):
-            for k in range(0, 3):
-                current = current and check(temp_store_colors[i][j][k], store_colors[i][j][k])
-
-
-    cv2.imshow("Checking", temp_store_colors)
-    # print("Current is ", current)
-    # print("Last was ", last)
-    # current = check(temp_b, temp_g, temp_r)
-
-    # print("Start x is", start_x)
-    # print("Start y is", start_y)
-    # print("End x is", end_x)
-    # print("End y is", end_y)
-    print("Checking")
-    if(current != last):
-        cnt += 1
-        print(frame[width//2, height//2])
-        print("Count increased")
-
-    last = current
-
-    frames_skipped = 0
-
-    k = cv2.waitKey(30)
-    if k == 27:
-        break
-
-
-print("The answer is", cnt//2)
-
-print("Count is", cnt)
 
