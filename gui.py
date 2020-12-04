4408e46a0b0373c9f1cf576ea9cc8e520f639312
enti-re <nikhilchandna2000@gmail.com>
2020-12-04 13:31:06 +0530

    Clean Code

--- gui.py
+++ gui.py
@@ -1,19 +1,15 @@
 import sys
-# from PyQt5.QtWidgets import QApplication
-# from PyQt5.QtWidgets import QLabel
 from PyQt5.QtGui import QImage, QIcon, QPixmap, QPalette, QBrush, QColor, QFontDatabase, QFont
-# from PyQt5.QtCore import *
 from PyQt5.QtWidgets import * 
-# from PyQt5.QtWidgets import QInputDialog, QLineEdit, QDialog
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore
 from test import *
-# from gui2 import *
 
 text=' '
 x=0
 msg=' '
+
 class AnotherWindow(QWidget):
     """
     This "window" is a QWidget. If it has no parent, it
@@ -22,112 +18,67 @@ class AnotherWindow(QWidget):
     def __init__(self):
         super(AnotherWindow,self).__init__()
         self.layout = QVBoxLayout()
-        # labe = QLabel("Result", sel/f)
-        # labe.setStyleSheet("color: #CFD8DC")
-        # labe.setFont(QFont('Fredoka One
-        # labe.move(105,95)
         calculation()
-        
         label = QLabel("Your result is : {}".format(msg), self)
         label.setStyleSheet("color: #CFD8DC")
         label.setStyleSheet("color: #CFD8DC")
-        
         label.move(185,95)
-
-        label.setFont(QFont('Fredoka One', 29))
+        label.setFont(QFont('Grandstander Black', 29))
         self.setStyleSheet("""
         background-color: #5D6D7E;
-        
         """)
         label.adjustSize()
-
-        self.setWindowTitle("Chair Stand test")
+        self.setWindowTitle("Chair Stand test Result")
         self.setLayout(self.layout)
-        # self.label = QLabel("Another Window")
-        # self.layout.addWidget(self.label)
-        # self.setLayout(layout)
-        print("pokemon")
 
-# def show_new_window():
-#         # app = QApplication(sys.argv)
-#         w = AnotherWindow()
-#         w.resize(700,450)
-#         w.setFixedSize(700, 450)
-#         w.show()
-#         print("hill2")   
     def closeEvent(self, event):
-		# reply = QMessageBox.question(self, 'Window Close', 'Are you sure you want to close the window?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         sys.exit(app.exec_())
 
-        
-
 class MainWindow(QWidget):
-
-    
-    
+  
     def __init__(self):
         super(MainWindow, self).__init__()
         self.layout = QVBoxLayout()
         label = QLabel("Geriatric Agility Detection", self)
         label.setStyleSheet("color: #CFD8DC")
         label.move(105,105)
-
-        label.setFont(QFont('Fredoka One', 29))
+        label.setFont(QFont('Grandstander Black', 29))
         self.setStyleSheet("""
         background-color: #5D6D7E;
-        
         """)
         label.adjustSize()
-
-        self.setWindowTitle("My Own Title")
+        self.setWindowTitle("Chair Stand Test")
         self.setLayout(self.layout)
-
         self.Button1()
         self.Button2()
-        self.Button3()
-        # self.ui()
+           
     def getText(self):
+        font = QFont()
+        font.setFamily("Grandstander Black")
+        font.setPointSize(10)
         global text
-        text, okPressed = QInputDialog.getText(self, "Get text","Your gender and age: \n eg:f24", QLineEdit.Normal, "")
+        inputDialog = QInputDialog(None)
+        inputDialog.setInputMode(QInputDialog.TextInput)
+        inputDialog.setWindowTitle('Enter Your Details')
+        inputDialog.setLabelText("Your gender and age in format(given below)   \n eg:f24")
+        inputDialog.setFont(font)
+        inputDialog.setStyleSheet("""
+        background-color: #5D6D7E;
+        color: #17202A
+        """)
+        okPressed=inputDialog.exec()
+        text=inputDialog.textValue()
         if okPressed and text != '':
             global x
             x = register_Background()
-            print(cnt)
-            print("count is above ")
             self.popup = AnotherWindow()
-            # self.popup.resize(700,450)
             self.popup.setFixedSize(1000, 200)
             self.popup.show()
             
-            
-            # def board():
-            #     self.popup.show()
-           
-        # print("hill")
-    
-
-           
-           
-
-
-        
-    # def ui(self)
-    #     self.radioButton_male = QtWidgets.QRadioButton(self.centralwidget) 
-    #     self.radioButton_female = QtWidgets.QRadioButton(self.centralwidget)
-
-    #  def maleselected(self, selected): 
-    #     if selected: 
-    #         self.label.setText("You are male") 
-              
-    # def femaleselected(self, selected): 
-    #     if selected: 
-    #         self.label.setText("You are female")  
-
     def Button1(self): 
-    
         button = QPushButton("Start", self)
-        button.move(290,185)
-        button.setFont(QFont("Fredoka One",13.5))
+        button.move(290,195)
+        button.setFont(QFont("Grandstander Black",13.5))
         button.setStyleSheet("""
         background-color: #80CBC4;
         color: #FDFEFE;
@@ -136,47 +87,20 @@ class MainWindow(QWidget):
         """)
         button.clicked.connect(self.getText)
 
-
     def Button2(self): 
-
-        button = QPushButton("Guide", self) 
-        button.setFont(QFont("Fredoka One",13.5))
-        button.setFixedSize(100,35)
-        button.move(290,240)
-        button.setStyleSheet("""
-        background-color: #80CBC4;
-        color: #FDFEFE;
-        border-radius: 5px;
-        """)
-
-        
-
-        button.clicked.connect(self.clickme)
-
-    def Button3(self): 
-
         button = QPushButton("Exit", self) 
-        button.setFont(QFont("Fredoka One",13.5))
+        button.setFont(QFont("Grandstander Black",13.5))
         button.setFixedSize(100,35)
-        button.move(290,295)
+        button.move(290,245)
         button.setStyleSheet("""
         background-color: #E57373;
         color: #FDFEFE;
         border-radius: 5px;
         """)
+        button.clicked.connect(self.exit) 
 
-        
-
-        button.clicked.connect(self.clickme) 
-
-    def clickme(self): 
-        # printing pressed 
-        print("pressed")
-
-    def registerBackground(self): 
-        # printing pressed 
-        print("pressed")
-
+    def exit():
+        sys.exit(app.exec_())
 
 def calculation():
     gender=text[0]
@@ -187,33 +111,25 @@ def calculation():
     male = [(14, 19), (12, 18), (12, 17), (11, 17), (10, 15), (8, 14), (7, 12)]
     female = [(12,17),(11,16),(10,15),(10,15),(9,14),(8,13),(4,11)]
     index = (age-60)//5
-    print(index)
     if gender=='m':
         lower_bound = male[index][0]
         upper_bound = male[index][1]
     else:
         lower_bound = female[index][0]
         upper_bound = female[index][1]
-    print(lower_bound)
-    print(upper_bound)
 
-   
-    global msg   
-    print(x//2)
+    global msg
+       
     if(x//2 < lower_bound):
-        msg="less than average"
+        msg="Less Than Average"
     elif(x//2 > upper_bound):
-        msg="more than average"
+        msg="More Than Average"
     else:
-        msg="average"
-
-    print(msg)
-
+        msg="Average"
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mw = MainWindow()
-    print("Cnt in main is {}".format(cnt))
     mw.resize(700,450)
     mw.setFixedSize(700, 450)
     mw.show()
--- test.py
+++ test.py
@@ -5,14 +5,11 @@ import threading
 import sys
 import os
 import trace
-# from gui import *
 
-# cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) #captureDevice = camera
 box_range = 25
 n = box_range*2+10
 check_range = 40
-# last = True
 cnt=0
 
 def check(x, y):
@@ -22,9 +19,7 @@ def check(x, y):
         return False
 
 store_colors = [[[]*n]*n]*3
-
 _, frame = cap.read()
-# cap.release()
 height, width, channels = frame.shape
 
 start_x = width//2 - box_range
@@ -33,24 +28,19 @@ start_y = height//2 - box_range
 end_y = height//2 + box_range
 start_y -= 200
 end_y -= 200
-
 t1=t2=0
 
-# print(last)
-
-
 def register_Background():
-    
-    
+
     while True:
         _, frame = cap.read()
         cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (157, 179, 69), 2)
         
         cv2.imshow("panel", frame)
        
-        # font = cv2.Ar
+      
         store_colors = frame[ start_y:(end_y+1) , start_x:(end_x+1), 0:3]
-        # cv2.putText('Nikhil',(50,50)) 
+       
         k = cv2.waitKey(30)
         if k == 27:
             print("Background registered")
@@ -67,50 +57,17 @@ def register_Background():
             break
     cv2.destroyAllWindows()
     print("Timer started")
+
     global t1
     global t2
-    # t1 = thread_with_trace(target=Timer)
-    # t2 = thread_with_trace(target=loop3,args=(store_colors, ))
     t1 = threading.Thread(target=Timer)
     t2 = threading.Thread(target=loop3,args=(store_colors, ))
     t1.start()
     t2.start()
     t1.join()
     t2.join()
-    print("Cnt in end of register background is {}".format(cnt))
     return cnt
     
-    
-# def Timer():
-#     i=-1;
-#     l_time=True
-#     last_time=time.time()
-#     font = cv2.FONT_HERSHEY_SIMPLEX 
-#     org = (50, 50) 
-#     fontScale = 1 
-#     color = (101, 138, 255) 
-#     thickness = 2
-#     image=cv2.imread("download.png")
-#     cv2.imshow("panel",image)
-    
-
-#     while(True):
-#         image = cap.read()
-#         # cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), (157, 179, 69), 2)
-#         # cv2.imshow("panel", img)
-#         # curr_time = time.time()
-#         # # print("Seconds since epoch =", curr_time)	
-#         # if(curr_time-last_time>=1):
-#         #     l_time=True
-#         # if(l_time==True):
-#         #     i=i+1
-#         #     last_time=curr_time
-#         #     l_time=False
-#         i=i+1
-#         image=cv2.putText(image,str(i), org, font,  
-#             fontScale, color, thickness, cv2.LINE_AA) 
-#         cv2.imshow("panel", image)
-#         k = cv2.waitKey(0) & 0xFF
 def loop3(store_colors):
     last = True
     global cnt
@@ -138,29 +95,18 @@ def loop3(store_colors):
         k = cv2.waitKey(30)
         if k == 27:
             break
-    # loop3(store_colors)
     print("The answer is", cnt//2)
     print("Count is", cnt)
     cv2.destroyAllWindows()
-#         if k == 27:  
-#             cv2.destroyAllWindows() 
-      
-
 
 def Timer():
-   
-    # Check if camera opened successfully 
     cap = cv2.VideoCapture('timer.mp4')
-       
     if (cap.isOpened()== False):  
         print("Error opening video  file") 
-
-    while(cap.isOpened()): 
-        
+    while(cap.isOpened()):    
     # Capture frame-by-frame 
         ret, frame = cap.read() 
         if ret == True: 
-        
             # Display the resulting frame 
             cv2.imshow('Frame', frame) 
             # fps = frame.get(cv2.cv.CV_CAP_PROP_FPS)
@@ -169,27 +115,11 @@ def Timer():
             if cv2.waitKey(25) & 0xFF == ord('q'): 
                 break 
         else:  
-              break
-              
+              break            
     # When everything done, release  
     # the video capture object 
-    cap.release() 
-    
+    cap.release()     
     # Closes all the frames 
     cv2.destroyAllWindows() 
  
 
-     
-        # def final():
-#     t1 = threading.Thread(target=loop3)
-#     t2 = threading.Thread(target=Timer) 
-  
-#     # starting thread 1 
-#     t1.start() 
-#     # starting thread 2 
-#     t2.start() 
-  
-#     # wait until thread 1 is completely executed 
-#     t1.join() 
-#     # wait until thread 2 is completely executed 
-#     t2.join() 
